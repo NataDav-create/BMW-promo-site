517717e21325c9b6b1b7b67def2379aefc716977
Natalia Davydova <davydovanatalia063@gmail.com>
2021-05-18 13:33:37 -0700

    add accordeon function

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,31 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const featureLinkslems = document.querySelectorAll('.feature__link');
+  const featureLinksElems = document.querySelectorAll('.feature__link');
   const featureSubElems = document.querySelectorAll('.feature-sub');
 
-
-  // featureLinkslems.forEach((elem, i) => {
-  //   elem.classList.remove('feature__link_active');
-  //   closeAllDrops();
-  //   elem.addEventListener('click', () => {
-  //     closeAllDrops();
-  //     if (elem.classList.contains('feature__link_active')) {
-  //       elem.classList.remove('feature__link_active');
-  //     } else {
-  //       elem.classList.add('feature__link_active');
-  //       featureSubElems.forEach((item, index) => {
-  //         if (elem.classList.contains('feature__link_active') && (index === i)) {
-  //           item.classList.remove('hidden')
-  //         } else {
-  //           item.classList.add('hidden')
-  //         }
-  //       })
-  //     }
-  //   })
-  // });
-
-  // function closeAllDrops() {
-  //   featureSubElems.forEach((elem) => {
-  //     elem.classList.add('hidden')
-  //   });
-  // }
+  featureLinksElems.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      featureSubElems.forEach((elem) => {
+        elem.classList.add('hidden')
+      });
+      featureLinksElems.forEach((elem) => {
+        elem.classList.remove('feature__link_active')
+      })
+      featureSubElems[index].classList.remove('hidden');
+      btn.classList.add('feature__link_active');
+    })
+  })
 });
